14b736a0e669ec0a5f4f753497a77a5c9485a4fe
jdinh <52547731+jdinh-782@users.noreply.github.com>
2021-04-24 18:26:46 -0700

    Add files via upload

---
+++ FlyingCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingCode : MonoBehaviour
+{
+    public GameManager gameManager;
+    
+    public float velocity = 1;
+
+    private Rigidbody2D _obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _obj = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //start the flying motion of the bird (left mouse click)
+        if (Input.GetMouseButtonDown(0))
+        {
+            _obj.velocity = Vector2.up * velocity;
+        }
+    }
+
+    //call when object hits a pipe to show the game over method
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
+    }
+}
---
+++ SpawnPipe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPipe : MonoBehaviour
+{
+    public float timeToSpawn;
+    private float _timer = 0;
+    public float height;
+    public GameObject pipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPipe = Instantiate(pipe);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //create a new pipe after a certain amount of time has passed (preferably 1 second)
+        if (_timer > timeToSpawn)
+        {
+           GameObject newPipe = Instantiate(pipe);
+           newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+
+           Destroy(newPipe, 15);
+           _timer = 0; 
+        }
+        _timer += Time.deltaTime;
+    }
+    
+}
